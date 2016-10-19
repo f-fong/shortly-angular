df85f18a6f72e658875c8428972c80236e085136
Felicia Fong <feliciasfong@gmail.com>
2016-10-18 17:08:00 -0700

    Built out Links, LinksController, ShortenController and routing templates, had trouble with Navigation and used course solution

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,19 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+
+  //$scope.data = {links: ....}
+
+  $scope.getLink = function() {
+  	Links.getAll().then(function(links) {
+  		$scope.data.links = links; 
+  	})
+  }; 
+
+  $scope.getLink(); 
+
+  
 });
+
+
